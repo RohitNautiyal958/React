28f2cd46403082aba479502616ee9d9f11033313
Rohit Nautiyal <87179238+RohitNautiyal958@users.noreply.github.com>
2023-05-03 11:37:14 +0530

    Add files via upload

---
+++ Condistionrender.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export default function Condistionrender(props) {
+
+    
+  return (
+    <div>
+      <h1>RN</h1>
+
+    <button onClick={props.getdata}>Call the alert</button>
+    </div>
+  )
+}
